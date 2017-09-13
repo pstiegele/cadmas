69736ef76126cb879d916f57a4ce1473b6f057af
pstiegele <paul@stiegele.name>
2017-09-13 22:00:36 +0200

    chat app commit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.send('Hello World, 123!');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(8081, function () {
-  console.log('Example app listening on port 80!');
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(8081, function () {
+    console.log('listening on *:8081');
 });
