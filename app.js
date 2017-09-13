9915cde946f8ec14b335c8b91785849878e6c17b
pstiegele <paul@stiegele.name>
2017-09-13 20:42:53 +0200

    b

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World, 123!');
 });
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 80!');
 });
