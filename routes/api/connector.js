01847e1121de63de4a8ed21cc2754ddcb1a2d2b7
pstiegele <paul@stiegele.name>
2017-10-19 22:56:22 +0200

    socket io java anpassung

--- routes/api/connector.js
+++ routes/api/connector.js
@@ -3,17 +3,24 @@ module.exports = function(io){
   var router = app.Router();
 
   io.on('connection', function(socket){
-    console.log("hi, hat funktioniert!");
+    console.log("Client connected");
+    socket.emit('test', { hello: 'world' });
+
   });
 
-  router.get('/', function(req, res, next) {
-    // io.on('connection', function(socket){
-    //   console.log('user connected');
-    // });
-
-
-    res.send('<html><head></head><body><script src=\"/socket.io/socket.io.js\"></script><script>var socket = io();</script></body></html>');
+  io.on('foo', function (data) {
+    console.log(data);
   });
+
+  //
+  // router.get('/', function(req, res, next) {
+  //   // io.on('connection', function(socket){
+  //   //   console.log('user connected');
+  //   // });
+  //
+  //
+  //   res.send('<html><head></head><body><script src=\"/socket.io/socket.io.js\"></script><script>var socket = io();</script></body></html>');
+  // });
 
 
   return router;
