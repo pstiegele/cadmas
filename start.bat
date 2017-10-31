2d31caa8af2efc7432048abfadbb97ea2c5e856d
pstiegele <paul@stiegele.name>
2017-10-31 21:57:52 +0100

    use environment port variable for http

--- start.bat
+++ start.bat
@@ -10,6 +10,7 @@ set Path=%NodePackagesPath%;%PATH%
 set NODE_PATH=%NodePackagesPath%\node_modules;%NODE_PATH%
 set NODE_ENV=debug
 set PORT=443
+set HTTPPORT=80
 set DB_HOST=cadmas.cegpb2uou7a1.eu-west-1.rds.amazonaws.com
 set DB_USER=server_user
 set DB_PASSWORD=eua100DPME
