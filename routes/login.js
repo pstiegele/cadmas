9a3b0b1b1d2dd297523ea011988306b60a9d42e2
pstiegele <paul@stiegele.name>
2017-11-06 23:37:19 +0100

    bcrypt eingeführt, passwort wird jetzt verschlüsselt und mit salt gespeichert

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 var qs = require('querystring');
+var bcrypt = require('bcrypt');
 
 
 router.get('/', function(req, res, next) {
@@ -10,12 +12,49 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
-  res.render('login', {
-    user: "Welcome, "+req.body.user+"!",
-    password: "Your password is: "+ req.body.password
+  userAndPasswordisValid(req.body.user, req.body.password, function(authorized) {
+    if (authorized) {
+      res.render('login', {
+        user: "Welcome, " + req.body.user + "!",
+        password: "Your password is: " + req.body.password
+      });
+    } else {
+      res.render('login', {
+        //user: bcrypt.hashSync('123', 15),
+        user: "I can't find your username, "+req.body.user+"!",
+        password: "Your password " + req.body.password + " is wrong."
+      });
+    }
+
   });
 
 
 });
+
+
+function userAndPasswordisValid(user, password, callback) {
+  var db = require('../tools/db')();
+  if (user && password) {
+    var query = "SELECT * FROM User WHERE username=?";
+    db.query(query, user, function(err, results) {
+      if (err === null && results.length == 1) {
+        var pa = password;
+        var respas = results[0].password;
+        bcrypt.compare(password, results[0].password, function(err, res) {
+          if (res) {
+            callback(true);
+          } else {
+            callback(false);
+          }
+        });
+
+      } else {
+        callback(false);
+      }
+    });
+  }else{
+    callback(false);
+  }
+}
 
 module.exports = router;
