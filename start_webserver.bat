00f76273915ff838dcfc4006ea7f3031f605e828
pstiegele <paul@stiegele.name>
2018-03-04 22:54:07 +0100

    ahh, we need only one http server, nice!

---
+++ start_webserver.bat
@@ -0,0 +1,24 @@
+@echo off
+echo.
+set NodePackagesPath=C:\Program Files\nodejs
+
+set Path=%NodePackagesPath%\node_modules\.bin;%PATH%
+set Path=%NodePackagesPath%;%PATH%
+
+set NODE_PATH=%NodePackagesPath%\node_modules;%NODE_PATH%
+set NODE_ENV=debug
+set DB_HOST=cadmas.cegpb2uou7a1.eu-west-1.rds.amazonaws.com
+set DB_USER=server_user
+set DB_PASSWORD=eua100DPME
+set DB_PORT=3306
+set DB_DB=cadmas
+set SETUPHTTPSERVER=true
+set HTTPPORT=80
+set HTTPSPORT=443
+set APIPORT=3000
+set JWTSECRET=E1hf7FduVtAm9nQQCYa9
+
+
+
+npm run dev-webserver
+pause
